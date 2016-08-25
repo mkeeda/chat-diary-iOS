021781ce4a6d94a5bdc3150a78727cd45cdba1d9
IppeiMUKAIDA <mr.sunege@gmail.com>
2016-08-26 06:13:33 +0900

    SDWebImageをインストール

---
+++ InternDiary/EntryBodyCell.swift
@@ -0,0 +1,13 @@
+//
+//  EntryBodyCell.swift
+//  InternDiary
+//
+//  Created by IppeiMUKAIDA on 8/26/16.
+//  Copyright © 2016 IppeiMUKAIDA. All rights reserved.
+//
+
+import Cocoa
+
+class EntryBodyCell: UICollectionViewCell {
+
+}
